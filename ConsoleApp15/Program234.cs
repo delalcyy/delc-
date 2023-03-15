6964ef25856cf06b69f3353b5d1e421054ce2727
Delal Ulçay <127891281+delalcyy@users.noreply.github.com>
2023-03-15 13:52:36 +0300

    sayilar2
    proje

---
+++ ConsoleApp15/Program234.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            int yazilacakrakam;
+            Console.Write("Girilecek sayiyi yaziniz: ");
+            sayi= Convert.ToInt32(Console.ReadLine());  
+            while(sayi > 10)
+            {
+                yazilacakrakam = sayi % 10;
+                Console.Write(yazilacakrakam);
+                sayi /= 10;
+            }
+            Console.WriteLine(sayi);
+            Console.ReadKey();
+
+        }
+    }
+}
